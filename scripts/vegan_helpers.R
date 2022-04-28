324a8a2e4c7f9db5157b12b59d8fbd27480b949c
jebyrnes <jarrett.byrnes@umb.edu>
2022-04-28 15:05:58 -0400

    put jaccard workflow in vegan helpers

--- scripts/vegan_helpers.R
+++ scripts/vegan_helpers.R
@@ -37,3 +37,34 @@ remove_null_rows <- function(adf){
   adf[-idx,]
 }
 
+
+make_jaccard_timeseries <- . %>%
+  group_by(site, intertidal_transect, 
+           replicate, year, exposure, level, height, organism) %>%
+  summarize(value = sum(value, na.rm=TRUE)) %>%
+  ungroup() %>%
+  group_by(site, intertidal_transect, 
+           year, exposure, height, organism) %>% #height as the group
+  summarize(value = mean(value, na.rm=TRUE)) %>%
+  ungroup() %>%
+  pivot_wider(values_from = value, names_from = organism,
+              values_fill = 0)  %>% #assume missing = 0
+  group_by(site, intertidal_transect, 
+           exposure, height) %>%
+  arrange(year) %>%
+  nest() %>%
+  mutate(
+    data = purrr::map(data, add_data_year_rownames), #get it into vegan
+    data = purrr::map(data, remove_null_cols), #remove null species
+    data = purrr::map(data, remove_null_rows), #remove years in bare patches
+    jaccard_mat = purrr::map(data, ~1 - 
+                               as.matrix(
+                                 vegdist(.x, method = "jaccard"))),
+    j = purrr::map(jaccard_mat,
+                   ~data.frame(year = rownames(.x),
+                               jaccard = .x[,1]
+                   ))) %>%
+  unnest(j) %>%
+  mutate(year = as.numeric(year))%>% 
+  filter(year != 1986) #get rid of 1st year to reduce bias
+
